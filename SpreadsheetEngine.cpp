365e3e7e0cf86fa103e037fb3586e687b7537c7c
Josh Staples <jwstaples2@gmail.com>
2026-07-14 19:51:07 -0400

    initial project creation

---
+++ SpreadsheetEngine.cpp
@@ -0,0 +1,12 @@
+﻿// SpreadsheetEngine.cpp : Defines the entry point for the application.
+//
+
+#include "SpreadsheetEngine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
